2b8e9ffcc70a7986e6d34e17aee410044b8896df
2017-A-15 <ea_pain@yahoo.com>
2020-06-24 12:55:27 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+'''
+References:
+          https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+          https://developer.ibm.com/technologies/blockchain/tutorials/develop-a-blockchain-application-from-scratch-in-python/
+          https://www.youtube.com/watch?v=KARxDX5DTgY
+          https://www.youtube.com/watch?v=b81Ib_oYbFk
+          https://www.activestate.com/blog/how-to-build-a-blockchain-in-python/
+          https://dzone.com/articles/how-to-create-your-own-cryptocurrency-blockchain-i
+          https://medium.com/@OpcitoTechnologies/create-your-own-blockchain-using-python-and-ethereum-9153696f2187
+          https://medium.com/coinmonks/python-tutorial-build-a-blockchain-713c706f6531
+          https://www.freecodecamp.org/news/create-cryptocurrency-using-python/
+          https://101blockchains.com/build-a-blockchain-in-python/
+          https://towardsdatascience.com/building-a-minimal-blockchain-in-python-4f2e9934101d
+'''
